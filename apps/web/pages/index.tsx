29941c09519d2cea5757eb059a8df49e985473a7
TiagoAMarek <Tiago_Marek@gap.com>
2023-04-01 15:03:36 -0300

    basic tailwindcss config

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -15,13 +15,16 @@ export default function Web() {
     undefined | PokemonResponse
   >();
 
-  const searchPokemon = useCallback(async (e) => {
-    e.preventDefault()
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-    );
-    setPokemonResponse(await response.json());
-  }, [pokemonName]);
+  const searchPokemon = useCallback(
+    async (e: { preventDefault: () => void; }) => {
+      e.preventDefault();
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+      );
+      setPokemonResponse(await response.json());
+    },
+    [pokemonName]
+  );
 
   return (
     <div>
@@ -33,14 +36,29 @@ export default function Web() {
             value={pokemonName}
             onChange={(e) => setPokemonName(e.target.value)}
           ></input>
-          <button type="submit" >buscar</button>
+          <button className="text-3xl font-bold underline" type="submit">buscar</button>
         </label>
       </form>
 
       {pokemonResponse ? (
         <div>
-          <h1>{pokemonResponse.name}</h1>
-          <Image src={pokemonResponse.sprites?.front_default ?? ''} alt={`image of ${pokemonResponse.name}`} width={100} height={100} />
+          <div className="p-6 max-w-sm mx-auto bg-black rounded-xl shadow-lg flex items-center space-x-4">
+            <div className="shrink-0">
+              <Image
+                src={pokemonResponse.sprites?.front_default ?? ""}
+                alt={`image of ${pokemonResponse.name}`}
+                className="h-12 w-12"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div>
+              <div className="text-xl font-medium text-black">
+                {pokemonResponse.name}
+              </div>
+              <p className="text-slate-500">You have a new message!</p>
+            </div>
+          </div>
         </div>
       ) : null}
     </div>
